62122eb9f6c75b4e306eff643d3279668ae824a5
Gam5510 <gameruz673@gmail.com>
2026-02-01 17:52:39 +0500

    Change id field to Optional in UserLite model

--- itdpy/models/user_lite.py
+++ itdpy/models/user_lite.py
@@ -2,10 +2,13 @@ from typing import Optional
 from pydantic import Field
 from .base import ITDBaseModel
 
+
 class UserLite(ITDBaseModel):
-    id: str
+    id: Optional[str] = None 
     username: Optional[str] = None
+
     display_name: Optional[str] = Field(None, alias="displayName")
     avatar: Optional[str] = None
     verified: bool = False
+
     is_following: bool = Field(False, alias="isFollowing")
